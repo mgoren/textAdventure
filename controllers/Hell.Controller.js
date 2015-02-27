88c7fb2984dd0e0486426340e14df828d756a3f0
Grace Mekarski and Mike Goren <gracelauren909@gmail.com>
2015-02-26 17:10:23 -0800

    add button to start again

--- controllers/Hell.Controller.js
+++
@@ -1,7 +0,0 @@
-rpg.controller('HellCtrl', function HellCtrl($scope, $state, UserFactory) {
-  $scope.user = UserFactory.user;
-  $scope.UserFactory = UserFactory;
-  $scope.beginAgain = function() {
-    $state.go('home');
-  };
-});
